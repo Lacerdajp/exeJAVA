1b143a301bceba6e4466587c4e74e1a7e2023742
João Pedro Lacerda <joaopedro.jplsouza@gmail.com>
2022-03-12 10:10:19 -0300

    push

--- Topico2/ExeCarro/Carro.java
+++ Topico2/ExeCarro/Carro.java
@@ -1,13 +1,33 @@
 package ExeCarro;
 
 public class Carro {
+
+    String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
     int vel = 0;
+    int tempo = 0;
+    int distanciaTotal = 0;
+    int distancia = 0;
 
     void aumentarVel(int vel) {
-
+        this.vel = vel + this.vel;
     }
 
     void diminuirVel(int vel) {
+        this.vel = vel + this.vel;
+    }
 
+    void percorrer(int tempo) {
+        this.distancia = tempo / this.vel;
+        distanciaPercorrida();
+    }
+
+    int distanciaPercorrida() {
+        this.distanciaTotal = this.distancia;
+        return this.distanciaTotal;
     }
 }
---
+++ Topico2/ExeCarro/Competicao.java
@@ -0,0 +1,7 @@
+package ExeCarro;
+
+public class Competicao {
+    int quantidadeCars;
+    Carro[] carros;
+
+}
